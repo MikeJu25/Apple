37fcb4ddbf21f6d4ea50a2efeb144fa43e719afd
MikeJu <feng0025@yahoo.com>
2020-06-14 18:59:51 -0700

    DatabaseUI, no longer using visual UI designer(2nd)

--- AppleDB/src/ui/DataBaseUI.java
+++ AppleDB/src/ui/DataBaseUI.java
@@ -7,13 +7,21 @@ import java.awt.event.ActionListener;
 
 public class DataBaseUI extends JFrame implements ActionListener {
     private JPanel panel;
-   // private JLabel filter;
+
     private JLabel yearTitle;
     private JLabel priceTitle;
+    private JLabel storage;
+    private JLabel color;
     private JComboBox price;
     private JComboBox year;
-    private JComboBox storage;
-    private JComboBox color;
+    private JCheckBox gold;
+    private JCheckBox gray;
+    private JCheckBox black;
+    private JCheckBox shiliu;
+    private JCheckBox liushisi;
+    private JCheckBox black;
+
+
 
     String[] strings = { "High to Low", "Low to High", "Interval"};
 
@@ -21,24 +29,26 @@ public class DataBaseUI extends JFrame implements ActionListener {
         panel = new JPanel(new GridLayout(3,1));
         add(panel);
 
-       //filter = new JLabel("Filter");
+
         priceTitle = new JLabel("Price");
         yearTitle = new JLabel("Year");
+        storage = new JLabel("Storage");
+        color = new JLabel("Color");
 
         price = new JComboBox(strings);
         price.setSelectedIndex(2);
         price.addActionListener(this);
 
         year = new JComboBox(strings);
-
         year.setSelectedIndex(2);
         year.addActionListener(this);
 
-       // panel.add(filter);
         panel.add(yearTitle);
         panel.add(year);
         panel.add(priceTitle);
         panel.add(price);
+        panel.add(color);
+        panel.add(storage);
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         add(panel, BorderLayout.CENTER);
--- AppleDB/src/ui/d.java
+++
@@ -1,9 +0,0 @@
-package ui;
-
-import javax.swing.*;
-
-public class d {
-    private JComboBox comboBox1;
-    private JEditorPane editorPane1;
-    private JTextPane textPane1;
-}
