c250e03c1346d1e2be1115c175b3caaebc23de2d
MikeJu <feng0025@yahoo.com>
2020-06-15 15:59:00 -0700

    original version for the connection of DBUI and Result table. Adding result class. Selection of price to be done 5

--- AppleDB/src/ui/model/Result.java
+++ AppleDB/src/ui/model/Result.java
@@ -8,6 +8,7 @@ public class Result {
 
 
    public static ArrayList<String> resultsAll = AppleProductDataBase.getPhoneInfo();
+    public static ArrayList<String> resultsPLTH = AppleProductDataBase.priceHighToLow();
 
     public static ArrayList<String> getResultAll() {
         ArrayList<String> results = AppleProductDataBase.getPhoneInfo();
