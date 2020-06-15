d7db6b02ba655e4f6a1df17593ccc096e5673c04
averyw0521 <wang0521@students.cs.ubc.ca>
2020-06-14 18:57:49 -0700

    LOGINUI UPDATE

--- AppleDB/src/ui/LoginUI.java
+++ AppleDB/src/ui/LoginUI.java
@@ -1,25 +1,83 @@
 package ui;
 
 
+import exception.TooLongName;
+
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.util.Objects;
 
-public class LoginUI extends JFrame {
+public class LoginUI extends JFrame implements ActionListener {
 
     static String userName;
-    private JLabel userNameLabel;
-    private JLabel passWordLabel;
-    private JButton submit;
-    private JTextField userNameText;
-    public JPanel panel;
-    private JTextField passwordText;
+    private JPanel panel;
+
+    private JLabel userLabel;
+    private JLabel passwordLabel;
+    private JLabel message;
+    private JLabel tooLongNameWarning;
     private JLabel emptyNameWarning;
+
+    private static JTextField userNameText;
+    private static JTextField passwordText;
+    private JButton submit;
+
+
+    LoginUI() {
+        panel = new JPanel();
+        add(panel);
+
+        userLabel = new JLabel();
+        userLabel.setText("       Username: ");
+        userNameText = new JTextField();
+        userNameText.setSize(300,100);
+
+        passwordLabel = new JLabel();
+        passwordLabel.setText("       Password: ");
+        passwordText = new JTextField();
+        passwordText.setSize(300,100);
+
+        tooLongNameWarning = new JLabel();
+        tooLongNameWarning.setText("Your username or password is incorrect. Please retry!");
+        emptyNameWarning = new JLabel("Please enter a valid combination of username and password!");
+
+        submit = new JButton("LOG IN");
+
+        panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+        panel.add(userLabel);
+        panel.add(userNameText);
+        panel.add(passwordLabel);
+        panel.add(passwordText);
+        panel.add(submit);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        submit.addActionListener( this);
+        add(panel, BorderLayout.CENTER);
+        pack();
+
+        setSize(new Dimension(400, 500));
+        setResizable(true);
+        setLocation(500, 300);
+        setTitle("Name Input");
+        setVisible(true);
+
+    }
+
+    public boolean checkNameLegal(String s) throws TooLongName {
+        //names = new ArrayList<String>();
+        if (s.length() > 10) {
+            throw new TooLongName();
+        } else {
+            return true;
+        }
+    }
 
     static void playSound(String soundName) {
         try {
@@ -33,22 +91,29 @@ public class LoginUI extends JFrame {
         }
     }
 
-    public LoginUI() {
 
-        submit.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                userName = userNameText.getText();
-                playSound("./data/buzzer.wav");
-                if (Objects.equals(userName, "")) {
-                    panel.add(emptyNameWarning);
-                }
+    // EFFECTS: When submit is clicked, if input is null, print the error message to the panel; if input is longer
+    //          than 10 characters, print the warning message to the panel; otherwise get into a new membership UI
+    public void actionPerformed(ActionEvent ae) {
+        userName = userNameText.getText();
+        playSound("./data/buzzer.wav");
+        if (Objects.equals(userName, "")) {
+            panel.add(emptyNameWarning);
+        }
+        try {
+            if (checkNameLegal(userName)) {
+                dispose();
+
+                //new MembershipUI(this);
+
             }
-        });
+        } catch (TooLongName tooLongName) {
+            // setSize(450, 350);
+            panel.add(tooLongNameWarning);
+            pack();
+//        } catch (RepeatedName repeatedName) {
+//           // repeatedName.printStackTrace();
+        }
+
     }
-    }
-
-
-
-
-
+}
--- AppleDB/src/ui/Main.java
+++ AppleDB/src/ui/Main.java
@@ -2,18 +2,13 @@ package ui;
 // how can customer choose the amount their food want to be
 
 
+import exception.TooLongName;
 import javax.swing.*;
 
 public class Main {
-
-
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("APPLE PRODUCT DATABASE");
-        frame.setContentPane(new LoginUI().panel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+    public static void main(String[] args) throws TooLongName {
 
         new LoginUI();
+
     }
 }
