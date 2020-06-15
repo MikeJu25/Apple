9d164e4905eb32dd8c717d1e3bf6caa5d6cf56b6
JIAJUN QI <771337750@qq.com>
2020-06-14 23:20:09 -0700

    tmp revert

--- AppleDB/src/SQL/AppleProductDataBase.java
+++ AppleDB/src/SQL/AppleProductDataBase.java
@@ -1,38 +1,85 @@
 package SQL;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.io.*;
+import java.math.BigDecimal;
+import java.sql.*;
+import java.util.*;
+
+
 
 public class AppleProductDataBase {
-    public static void withConnection(String user, String pass, SQLCustomerFUN<Connection> callback) throws SQLException {
-        System.out.println("Creating Connection...");
-        try (
+//    public static void withConnection(String user, String pass) throws SQLException {
+//        System.out.println("Creating Connection...");
+//        try (
+//                Connection connection = DriverManager.getConnection("localhost:3306/apple",
+//                        "root", "990428")) {
+//            connection.setAutoCommit(false);
+//            System.out.println("Creating Connection... Success");
+//            //callback.accept(connection);
+//        }
+//    }
+//    public static void main(String[]args){
+//        String insert_str = "insert into Discount values(?, ?, ?)";
+//
+//        System.out.println("Create Statement...");
+//        try (PreparedStatement stmt = connection.prepareStatement(insert_str)) {
+//            stmt.setString(1, discountName);
+//            stmt.setString(2, level);
+//            stmt.setString(3, applicability);
+//            System.out.println("Execute...");
+//            stmt.executeUpdate();
+//
+//            connection.commit();
+//            System.out.println("Changes Committed");
+//        }
+//
+//    }
+public static void main(String[] args) {
+    try (Connection cont = DriverManager.getConnection("jdbc:mysql://localhost:3306/apple?serverTimezone=UTC",
+            "root", "990428");
+         Statement stmt = cont.createStatement();
+    ) {
+//        String query2 = "insert into Discount values ('a', 'b', 'c')";
+//        System.out.println("The SQL statement is: " + query2 + "\n");
+//        int countInserted = stmt.executeUpdate(query2);
+//        System.out.println(countInserted + " records inserted.\n");
 
-            Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@loca　lhost:1522:ug", user, pass)) {
-            connection.setAutoCommit(false);
-            System.out.println("Creating Connection... Success");
-            callback.accept(connection);
+        String strSelect = "select level from Discount where level = '0.9'";
+        ResultSet rst = stmt.executeQuery(strSelect);
+        System.out.println("The records selected are:");
+        int rowCount = 0;
+        while (rst.next()) {   // Move the cursor to the next row, return false if no more row
+            //String dn = rst.getString("discountName");
+            String lev = rst.getString("level");
+            //String al = rst.getString("applicability");
+            //System.out.println(dn + ", " + lev + ", " + al);
+            System.out.println("  lev ");
+            ++rowCount;
         }
+        System.out.println("Total number of records = " + rowCount);
+
+    } catch (SQLException ex) {
+        ex.printStackTrace();
     }
+}
 
 
-    public static void addDiscount(Connection connection, String discountName, String level, String applicability) throws SQLException {
-        String insert_str = "insert into Discount values(?, ?, ?, ?)";
 
-        System.out.println("Create Statement...");
-        try (PreparedStatement stmt = connection.prepareStatement(insert_str)) {
-            stmt.setString(1, discountName);
-            stmt.setString(2, level);
-            stmt.setString(3, applicability);
-            System.out.println("Execute...");
-            stmt.executeUpdate();
-
-            connection.commit();
-            System.out.println("Changes Committed");
-        }
-    }
+//    public static void addDiscount(Connection connection, String discountName, String level, String applicability) throws SQLException {
+//        String insert_str = "insert into Discount values(?, ?, ?)";
+//
+//        System.out.println("Create Statement...");
+//        try (PreparedStatement stmt = connection.prepareStatement(insert_str)) {
+//            stmt.setString(1, discountName);
+//            stmt.setString(2, level);
+//            stmt.setString(3, applicability);
+//            System.out.println("Execute...");
+//            stmt.executeUpdate();
+//
+//            connection.commit();
+//            System.out.println("Changes Committed");
+//        }
+//    }
 }
 
 
