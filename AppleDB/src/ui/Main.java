34c78cdf68208de365c9a2f5306a0915d08f5f3a
MikeJu <feng0025@yahoo.com>
2020-06-15 19:40:49 -0700

    Filter Completed

--- AppleDB/src/ui/Main.java
+++ AppleDB/src/ui/Main.java
@@ -2,12 +2,13 @@ package ui;
 // how can customer choose the amount their food want to be
 
 
+import SQL.AppleProductDataBase;
+
 public class Main {
 
 
     public static void main(String[] args) {
         new LoginUI();
-        //new ComparisonSuperTable();
-        //new ModelInformationTable5();
+
     }
 }
--- AppleDB/src/ui/model/Result.java
+++ AppleDB/src/ui/model/Result.java
@@ -7,8 +7,11 @@ import java.util.ArrayList;
 public class Result {
 
 
+
+
    public static ArrayList<String> resultsAll = AppleProductDataBase.getPhoneInfo();
     public static ArrayList<String> resultsPLTH = AppleProductDataBase.priceHighToLow();
+    public static ArrayList<String> resultsYNTO64 = AppleProductDataBase.newestToOldest();
 
     public static ArrayList<String> getResultAll() {
         ArrayList<String> results = AppleProductDataBase.getPhoneInfo();
