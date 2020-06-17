abf3964446b614c9375b79bdb44c6b36d2fffd7b
MikeJu <feng0025@yahoo.com>
2020-06-17 13:15:59 -0700

    All discountUI finished. Admin finished. Wait for more options (integrated result product table finished)

--- AppleDB/src/ui/ResultTableProducts.java
+++ AppleDB/src/ui/ResultTableProducts.java
@@ -1,5 +1,6 @@
 package ui;
 
+import SQL.AppleProductDataBase;
 import ui.model.Result;
 
 import javax.swing.*;
@@ -8,12 +9,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-public class DiscountResultTable extends JFrame implements ActionListener {
-
+public class ResultTableProducts extends JFrame implements ActionListener{
+    //public Result result = new Result();
+   // private static ArrayList<String> results = new ArrayList<String>();
     ArrayList<JButton> labels;
 
-    DiscountResultTable(ArrayList<String> results, String titlte){
-
+    ResultTableProducts(ArrayList<String> results, String title){
 
         JPanel panel = new JPanel();
         add(panel);
@@ -23,12 +24,14 @@ public class DiscountResultTable extends JFrame implements ActionListener {
         JButton label3 = new JButton();
         JButton label4 = new JButton();
         JButton label5 = new JButton();
+        JButton label6 = new JButton();
         ArrayList<JButton> buttonsBase = new ArrayList<>();
         buttonsBase.add(label1);
         buttonsBase.add(label2);
         buttonsBase.add(label3);
         buttonsBase.add(label4);
         buttonsBase.add(label5);
+        buttonsBase.add(label6);
 
         Integer size = results.size();
         labels = new ArrayList<>();
@@ -49,15 +52,37 @@ public class DiscountResultTable extends JFrame implements ActionListener {
         setSize(new Dimension(300, 375));
         setResizable(false);
         setLocation(400, 270);
-        setTitle(titlte);
+        setTitle(title);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
 
+
+
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        dispose();
-        new AdministratorTable();
+        JButton button = (JButton) e.getSource();
+        String str = button.getText();
+        if (str == "    Return     ") {
+            dispose();
+            new DataBaseUI();
+        }
+        if (str.contains("10001")) {
+            new ModelInformationTable1();
+        }
+        if (str.contains("10002")) {
+            new ModelInformationTable2();
+        }
+        if (str.contains("10003")) {
+            new ModelInformationTable3();
+        }
+        if (str.contains("10004")) {
+            new ModelInformationTable4();
+        }
+        if (str.contains("10005")) {
+            new ModelInformationTable5();
+        }
     }
 }
